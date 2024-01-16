3d23ac720803d516b32cdc15458869881972ff58
Nirbhay8672 <71960388+Nirbhay8672@users.noreply.github.com>
2024-01-16 22:05:53 +0530

    Set parent id in save plan

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -182,8 +182,9 @@ class AdminLoginController extends Controller
 		$user->fill([
 			'plan_id' => $request->plan_id,
 		])->save();
-
+		
 		Session::put('plan_id', $request->plan_id);
+		Session::put('parent_id', $user->id);
 		return redirect('/admin');
 	}
 }
