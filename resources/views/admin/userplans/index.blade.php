085bd5ae0da52c4939cf647c512fe10bf04e48fd
Nirbhay8672 <71960388+Nirbhay8672@users.noreply.github.com>
2024-04-03 23:17:16 +0530

    Set coupon modal

--- resources/views/admin/userplans/index.blade.php
+++ resources/views/admin/userplans/index.blade.php
@@ -9,9 +9,9 @@
             </div>
         </div>
         <!-- Container-fluid starts-->
+
         <div class="container-fluid">
             <div class="row">
-
                 <div class="col-sm-12">
                     <div class="card">
                         @if (Session::get('plan_expired_redirection'))
@@ -53,9 +53,15 @@
                                                     <input type="hidden" name="transaction_goal", value={{ $t_goal ?? 'new_subscription' }}>
                                                 @endif
 												<input type="hidden" name="plan_id" value="{{$plan->id}}">
-												<button class="btn btn-primary btn-lg" type="submit"
-                                                data-original-title="btn btn-primary btn-lg"
-                                                title="">Subscribe</button>
+
+												<button
+                                                    class="btn btn-primary btn-lg"
+                                                    type="button"
+                                                    data-original-title="btn btn-primary btn-lg"
+                                                    data-bs-toggle="modal"
+                                                    data-bs-target="#couponModal"
+                                                    onclick="setDetails({{ $plan->id }})"
+                                                >Subscribe</button>
 											</form>
                                         </div>
                                     </div>
@@ -67,9 +73,70 @@
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="couponModal" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-sm" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Coupon Form</h5>
+                        <button class="btn-close btn-light" type="button" data-bs-dismiss="modal" aria-label="Close"> </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{route('admin.savePlan')}}" method="post">
+                            @csrf
+                            <div class="form-row mb-2">
+                                <div class="form-group m-b-20">
+                                    <div class="fname">
+                                        <input
+                                            class="form-control"
+                                            type="text"
+                                            name="coupon_code"
+                                            id="coupon_code"
+                                            onkeydown="checkavailable()"
+                                            placeholder="Enter coupon code"
+                                        >
+                                        <input type="hidden" name="plan_id" class="form-control mt-2" id="plan_id">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-3 text-center">
+                                <div class="col">
+                                    <button class="btn custom-theme-button me-3" id="pay_with_coupon" disabled type="submit">Continue</button>
+                                </div>
+                            </div>
+                            <div class="row mb-3 text-center">
+                                <div class="col">
+                                    <button class="btn custom-theme-button me-3" id="pay_without_coupon" type="submit">I have No Coupon</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <!-- Container-fluid Ends-->
     </div>
 @endsection
 @push('scripts')
+
+<script>
+    let code = document.getElementById('coupon_code');
+    let button = document.getElementById('pay_with_coupon');
+    let button_without = document.getElementById('pay_without_coupon');
+
+    function checkavailable() {
+        button.disabled = code.value != '' ? false : true;
+        button_without.disabled = code.value != '' ? true : false;
+    }
+
+    function setDetails(value) {
+        let plan_id = document.getElementById('plan_id');
+        plan_id.value = value;
+        code.value = '';
+        button.disabled = true;
+        button_without.disabled = false;
+    }
+</script>
 
 @endpush
--- resources/views/guest/plan.blade.php
+++ resources/views/guest/plan.blade.php
@@ -52,7 +52,11 @@
     												<input type="hidden" name="user_id" value="{{ $user_id }}">
                                                     <button
                                                         class="btn btn-primary btn-lg"
-                                                        type="submit"
+                                                        type="button"
+                                                        data-original-title="btn btn-primary btn-lg"
+                                                        data-bs-toggle="modal"
+                                                        data-bs-target="#couponModal"
+                                                        onclick="setDetails({{ $plan->id }} , {{ $user_id }})"
                                                     >Subscribe</button>
                                                 @endif
 											</form>
@@ -66,5 +70,74 @@
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="couponModal" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-sm" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Coupon Form</h5>
+                        <button class="btn-close btn-light" type="button" data-bs-dismiss="modal" aria-label="Close"> </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{route('savePlan')}}" method="post">
+                            @csrf
+                            <div class="form-row mb-2">
+                                <div class="form-group m-b-20">
+                                    <div class="fname">
+                                        <input
+                                            class="form-control"
+                                            type="text"
+                                            name="coupon_code"
+                                            id="coupon_code"
+                                            onkeydown="checkavailable()"
+                                            placeholder="Enter coupon code"
+                                        >
+                                        <input type="hidden" name="plan_id" class="form-control mt-2" id="plan_id">
+                                        <input type="hidden" name="user_id" class="form-control mt-2" id="user_id">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-3 text-center">
+                                <div class="col">
+                                    <button class="btn custom-theme-button me-3" id="pay_with_coupon" disabled type="submit">Continue</button>
+                                </div>
+                            </div>
+                            <div class="row mb-3 text-center">
+                                <div class="col">
+                                    <button class="btn custom-theme-button me-3" id="pay_without_coupon" type="submit">I have No Coupon</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 @endsection
+
+
+@push('scripts')
+
+<script>
+    let code = document.getElementById('coupon_code');
+    let button = document.getElementById('pay_with_coupon');
+    let button_without = document.getElementById('pay_without_coupon');
+
+    function checkavailable() {
+        button.disabled = code.value != '' ? false : true;
+        button_without.disabled = code.value != '' ? true : false;
+    }
+
+    function setDetails(plan_id_value, user_id_value) {
+        let plan_id = document.getElementById('plan_id');
+        let user_id = document.getElementById('user_id');
+        plan_id.value = plan_id_value;
+        user_id.value = user_id_value;
+        code.value = '';
+        button.disabled = true;
+        button_without.disabled = false;
+    }
+</script>
+
+@endpush
