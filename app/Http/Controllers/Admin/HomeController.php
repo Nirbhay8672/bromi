6fe2aea8e518bb07b530a03a5c5993d20b29f976
mohdahsanshah <47567831+mohdahsanshah@users.noreply.github.com>
2024-08-11 12:32:32 +0530

    subtracted from total paid users

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1119,9 +1119,11 @@ class HomeController extends Controller
                     // add more users template
                     $extraUsers = $user->total_extra_users_added ?? 0;
                     $freeUsersAdded = $user->total_free_user ?? 0;
+                    $totalPaidUsers = $user->total_paid_user ?? 0;
                     $user->fill([
                         'total_extra_users_added' => $extraUsers + $orderTags['user_limit'],
                         'total_free_user' => $freeUsersAdded + $orderTags['user_limit'],
+                        'total_paid_user' => $totalPaidUsers - $orderTags['user_limit'],
                     ])->save();
                     $paymentInDb->update(['extra_users_added' => $orderTags['user_limit']]);
                 }
